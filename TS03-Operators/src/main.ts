a753e8171ae2961bf1752ea674ae5852a991de39
Matt | Varghese Mathew <16229800+mattvarghese@users.noreply.github>
2022-03-05 19:22:18 -0600

    operators

--- TS03-Operators/src/main.ts
+++ TS03-Operators/src/main.ts
@@ -4,19 +4,60 @@
 // GitHub: https://github.com/mattvarghese/typescript-tutorial
 
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
-// Assignment Operators
-let x = 90;
 
-// Arithmetic operators
+
+
+// Assignment Operator
+console.log("\n== Assignment Operator ==");
+let x: number;
+x = 90;
+console.log("x = 90: " + x);
+let str: string;
+str = "ABC";
+
+
+
+// Arithmetic Operators
+console.log("\n== Arithmetic Operators ==");
 // + - * /
-// %
+x = x + 10;
+console.log("x = x + 10: " + x);           // 100
+x = x - 1;
+console.log("x = x - 1: " + x);            // 99
+x = x / 9;
+console.log("x = x / 9: " + x);            // 11
+x = x * 4;
+console.log("x = x * 4: " + x);            // 44
+// % - Modulus / Remainder
+x = x % 10;
+console.log("x = x % 10: " + x);           // 4
 // + : for string concatenation
+str = str + "DEF";
+console.log('str = str + "DEF": ' + str);  // ABCDEF
 
-// Arithmetic assignment
-// += on strings
-// Also ++ --
+
+
+// Arithmetic Assignment Operators
+console.log("\n== Arithmetic Assignment Operators ==");
 x += 10;  // same as x = x + 10
-console.log(x);
+console.log("x += 10: " + x);              // 14
+// Also -=, *=, /=, %=
+// += on strings
+str += "GHI";
+console.log('str += "GHI": ' + str);       // ABCDEFGHI
+
+
+
+// Increment/Decrement Operators
+console.log("\n== Increment/Decrement Operators ==");
+console.log(`++x: ${++x}`);      // Prefix: change, then use
+console.log("x after ++x: " + x);
+console.log("--x: " + --x);      // Prefix: change, then use
+console.log("x after --x: " + x);
+console.log("x--: " + x--);      // Postfix: use, then chnge
+console.log("x after x--: " + x);
+console.log(`x--: ${x--}`);      // Postfix: use, then chnge
+console.log("x after x--: " + x);
 
 // Comparison operators
 
